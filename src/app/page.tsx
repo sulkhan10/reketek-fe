5cd7ca3e987ee575ffd9ffcb06cb860c3db1f3f2
sulkhan <sulkhangalangsakti@gmail.com>
2023-06-11 22:54:37 +0700

    feat : initialize hero homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import dynamic from "next/dynamic";
@@ -8,8 +8,59 @@ const DynamicNavbar = dynamic(() => import("@/components/Navbar"), {
 });
 export default function Home() {
   return (
-    <main className=" h-[100vh] bg-red-100  p-12 flex flex-col ">
-               <DynamicNavbar />
+    <main className=" h-[100vh] bg-red-100   flex flex-col ">
+      <DynamicNavbar />
+      <div>
+      <div>
+      <div>
+  <section
+    className="relative bg-cover bg-center bg-no-repeat"
+    style={{
+      backgroundImage: "url('https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}
+  >
+    <div className="absolute inset-0 bg-black opacity-75"></div>
+
+    <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+      <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+        <h1 className="text-3xl font-extrabold sm:text-5xl">
+          Let us find your
+          <strong className="block font-extrabold text-rose-700">
+            Forever Home.
+          </strong>
+        </h1>
+
+        <p className="mt-4 max-w-lg sm:text-xl/relaxed">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+          illo tenetur fuga ducimus numquam ea!
+        </p>
+
+        <div className="mt-8 flex flex-wrap gap-4 text-center">
+          <a
+            href="#"
+            className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+          >
+            Get Started
+          </a>
+
+          <a
+            href="#"
+            className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+          >
+            Learn More
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+</div>
+
+</div>
 
     </main>
   );
